d2a2b3875215eeec45b9453ef3626d53de9974c6
labulakalia <labulakalia@gmail.com>
2025-01-20 23:01:32 +0800

    add proto any

---
+++ plugin/plugin_test.go
@@ -0,0 +1,20 @@
+package plugin
+
+import (
+	"fmt"
+	"testing"
+
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+func TestPLugin(t *testing.T) {
+	formData := &AuthType_FormData{}
+	int32Value := wrapperspb.Int32(1111)
+	formData.Data, _ = anypb.New(int32Value)
+	fmt.Println("formData", formData.Data)
+
+	fmt.Println(formData.Data.GetTypeUrl())
+	// msg, _ := anypb.UnmarshalNew(formData.Data, proto.UnmarshalOptions{})
+	// fmt.Println(msg.(*wrapperspb.Int32Value).Value)
+}
