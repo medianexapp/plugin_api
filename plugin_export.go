d36894e9059974ce452f4c05c5d533855563360a
labulakalia <labulakalia@gmail.com>
2025-02-05 18:23:44 +0800

    fix wasmexport fail

--- plugin_export.go
+++ plugin_export.go
@@ -32,13 +32,13 @@ func err2Uint64(err error) uint64 {
 	return util.Uint32ToUint64(errPtr, uint32(errLength))
 }
 
-// go::wasmexport plugin_api_schema
+//go:wasmexport plugin_api_schema
 func plugin_api_schema() uint64 {
 	apiSchema, _ := pluginExport.PluginAPISchema()
 	return apiSchema
 }
 
-// go::wasmexport plugin_id
+//go:wasmexport plugin_id
 func plugin_id() uint64 {
 	id, err := pluginExport.PluginId()
 	if err != nil {
@@ -49,7 +49,7 @@ func plugin_id() uint64 {
 	return util.Uint32ToUint64(idPtr, idLen)
 }
 
-// go::wasmexport get_auth_type
+//go:wasmexport get_auth_type
 func get_auth_type() uint64 {
 	authType, err := pluginExport.GetAuthType()
 	if err != nil {
@@ -62,7 +62,7 @@ func get_auth_type() uint64 {
 	return util.Uint32ToUint64(uint32(util.BytesToPtr(data)), uint32(len(data)))
 }
 
-// go::wasmexport check_auth
+//go:wasmexport check_auth
 func check_auth(authTypePtr, authTypeLenPtr uint64) uint64 {
 	data := util.PtrToBytes(uint32(authTypePtr), uint32(authTypeLenPtr))
 	authType := &plugin.AuthType{}
@@ -77,7 +77,7 @@ func check_auth(authTypePtr, authTypeLenPtr uint64) uint64 {
 	return 0
 }
 
-// go::wasmexport get_auth_data
+//go:wasmexport get_auth_data
 func get_auth_data() uint64 {
 	authData, err := pluginExport.GetAuthData()
 	if err != nil {
@@ -86,7 +86,7 @@ func get_auth_data() uint64 {
 	return util.Uint32ToUint64(uint32(util.BytesToPtr(authData)), uint32(len(authData)))
 }
 
-// go::wasmexport check_auth_data
+//go:wasmexport check_auth_data
 func check_auth_data(raw_auth_dataPtr, raw_auth_dataLen uint64) uint64 {
 	rawAuthData := util.PtrToBytes(uint32(raw_auth_dataPtr), uint32(raw_auth_dataLen))
 	err := pluginExport.CheckAuthData(rawAuthData)
@@ -96,7 +96,7 @@ func check_auth_data(raw_auth_dataPtr, raw_auth_dataLen uint64) uint64 {
 	return 0
 }
 
-// go::wasmexport plugin_auth_id
+//go:wasmexport plugin_auth_id
 func plugin_auth_id() uint64 {
 	authId, err := pluginExport.PluginAuthId()
 	if err != nil {
@@ -105,7 +105,7 @@ func plugin_auth_id() uint64 {
 	return util.Uint32ToUint64(uint32(util.StringToPtr(&authId)), uint32(len(authId)))
 }
 
-// go::wasmexport get_dir_entry
+//go:wasmexport get_dir_entry
 func get_dir_entry(dir_pathPtr, dir_pathLen, page, page_size uint64) uint64 {
 	dir := util.PtrToString(uint32(dir_pathPtr), uint32(dir_pathLen))
 	dirEntry, err := pluginExport.GetDirEntry(dir, page, page_size)
@@ -119,7 +119,7 @@ func get_dir_entry(dir_pathPtr, dir_pathLen, page, page_size uint64) uint64 {
 	return util.Uint32ToUint64(uint32(util.BytesToPtr(dirEntryData)), uint32(len(dirEntryData)))
 }
 
-// go::wasmexport get_file_resource
+//go:wasmexport get_file_resource
 func get_file_resource(file_pathPtr, file_pathLen uint64) uint64 {
 	file_path := util.PtrToString(uint32(file_pathPtr), uint32(file_pathLen))
 	fileResource, err := pluginExport.GetFileResource(file_path)
@@ -133,8 +133,7 @@ func get_file_resource(file_pathPtr, file_pathLen uint64) uint64 {
 	return util.Uint32ToUint64(uint32(util.BytesToPtr(fileResourceData)), uint32(len(fileResourceData)))
 }
 
-// go::wasmexport close
+//go:wasmexport close
 func close() uint64 {
-
 	return err2Uint64(pluginExport.Close())
 }
