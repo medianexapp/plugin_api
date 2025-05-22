a9284a5d036f87e612ffacb4752b4f52ed48840f
labulakalia <labulakalia@gmail.com>
2025-05-22 10:07:10 +0800

    httpclient: add retry

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -2,6 +2,7 @@ package httpclient
 
 import (
 	"io"
+	"log/slog"
 	"net/http"
 	"net/url"
 	"strings"
@@ -17,19 +18,20 @@ func GetDefaultUserAgent() string {
 
 func (c *Client) setUserAgent(req *http.Request) {
 	if req.Header.Get("User-Agent") == "" {
-		req.Header.Set("User-Agent", c.userAgent)
+		req.Header.Set("User-Agent", c.option.userAgent)
 	}
 
 	return
 }
 
 type Client struct {
-	*http.Client
-	userAgent string
+	client *http.Client
+	option *Option
 }
 
 type Option struct {
 	userAgent string
+	retry     int
 }
 
 type FuncOption func(*Option)
@@ -40,23 +42,40 @@ func WithUserAgent(userAgent string) FuncOption {
 	}
 }
 
+func WithRetry(retry int) FuncOption {
+	return func(o *Option) {
+		o.retry = retry
+	}
+}
+
 func NewClient(opts ...FuncOption) *Client {
 	option := &Option{
 		userAgent: defaultUserAgent,
+		retry:     3,
 	}
 	for _, opt := range opts {
 		opt(option)
 	}
-
 	return &Client{
-		Client:    http.DefaultClient,
-		userAgent: option.userAgent,
+		client: http.DefaultClient,
+		option: option,
 	}
 }
 
-func (c *Client) Do(req *http.Request) (*http.Response, error) {
+func (c *Client) Do(req *http.Request) (resp *http.Response, err error) {
 	c.setUserAgent(req)
-	return c.Client.Do(req)
+	for range c.option.retry {
+		resp, err = c.client.Do(req)
+		if err != nil {
+			slog.Error("client do failed", "err", err)
+			continue
+		}
+		break
+	}
+	if err != nil {
+		return nil, err
+	}
+	return
 }
 func (c *Client) Get(url string) (resp *http.Response, err error) {
 	req, err := http.NewRequest("GET", url, nil)
@@ -64,7 +83,7 @@ func (c *Client) Get(url string) (resp *http.Response, err error) {
 		return nil, err
 	}
 	c.setUserAgent(req)
-	return c.Client.Do(req)
+	return c.Do(req)
 }
 func (c *Client) Head(url string) (resp *http.Response, err error) {
 	req, err := http.NewRequest("HEAD", url, nil)
@@ -72,7 +91,7 @@ func (c *Client) Head(url string) (resp *http.Response, err error) {
 		return nil, err
 	}
 	c.setUserAgent(req)
-	return c.Client.Do(req)
+	return c.Do(req)
 }
 func (c *Client) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
 	req, err := http.NewRequest("POST", url, body)
@@ -82,7 +101,7 @@ func (c *Client) Post(url string, contentType string, body io.Reader) (resp *htt
 	req.Header.Set("Content-Type", contentType)
 	c.setUserAgent(req)
 
-	return c.Client.Do(req)
+	return c.Do(req)
 }
 func (c *Client) PostForm(url string, data url.Values) (resp *http.Response, err error) {
 	return c.Post(url, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
